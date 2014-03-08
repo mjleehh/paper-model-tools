83128c0670b00b27be4eba7a11697f67e8b523bf
Michael Jonathan Lee <mj.lee@gmx.net>
2014-03-08 12:58:36 +0100

    Created file for selection state.

---
+++ selection.py
@@ -0,0 +1,16 @@
+class Selection:
+    """ Save and restore the selection. """
+
+    def __init__(self):
+        """ Store the current selection. """
+        selection = om.MSelectionList()
+        om.MGlobal.getActiveSelectionList(selection)
+        self.selection = selection
+        self.componentSelectionMask = om.MGlobal.componentSelectionMask()
+        self.selectionMode = om.MGlobal.selectionMode()
+
+    def makeCurrent(self):
+        """ Replace the current selection with the stored one. """
+        om.MGlobal.setSelectionMode(self.selectionMode)
+        om.MGlobal.setComponentSelectionMask(self.componentSelectionMask)
+        om.MGlobal.setActiveSelectionList(self.selection)
