1be0343053ecadd4899e7414e9db6cf221a69760
Michael Jonathan Lee <mj.lee@gmx.net>
2014-03-10 23:55:30 +0100

    Created a common base class for states.

---
+++ unfolder/select_facetree/states/state.py
@@ -0,0 +1,44 @@
+class State:
+    def __init__(self, context, previous):
+        self._context = context
+        self._previous = previous
+
+    # methods
+
+    def ffwd(self):
+        nextState = self._nextState()
+        if nextState:
+            return nextState()
+        else:
+            return self.reset()
+
+    def reset(self):
+        self._context.setHelpString(self._helpString())
+        self._waitForInput()
+        return self
+
+    # event callbacks
+
+    def doPress(self, event):
+        print('nothing do press')
+        return self
+
+    def doDrag(self, event):
+        print('nothing do drag')
+        return self
+
+    def doRelease(self, event):
+        print('nothing do release')
+        return self
+
+    def delete(self):
+        print('nothing delete')
+        return self._previous()
+
+    def complete(self):
+        print('nothing complete')
+        return self
+
+    def abort(self):
+        print('nothing abort')
+        return self
