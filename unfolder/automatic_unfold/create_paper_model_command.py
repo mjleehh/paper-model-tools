44bd9a53444184bc81879f7d6e528822f2cda2e8
Michael Jonathan Lee <mj.lee.hh@googlemail.com>
2014-03-16 13:31:15 +0100

    Dissembled paper model creation for all selected meshes and the creation of each paper model.

--- unfolder/automatic_unfold/create_paper_model_command.py
+++ unfolder/automatic_unfold/create_paper_model_command.py
@@ -23,27 +23,26 @@ class CreatePaperModelCommand(omp.MPxCommand):
         om.MGlobal.getActiveSelectionList(activeSelection)
 
         selectedObjects = om.MItSelectionList(activeSelection, om.MFn.kMesh)
-        self._flattenObjectsInSelectionList(selectedObjects, strategy)
+        self._flattenObjectsInSelectionList(selectedObjects)
 
         objectsWithSelectedFaces = om.MItSelectionList(activeSelection, om.MFn.kMeshPolygonComponent)
-        self._flattenObjectsInSelectionList(objectsWithSelectedFaces, strategy)
+        self._flattenObjectsInSelectionList(objectsWithSelectedFaces)
 
         print('flattening selection ... done')
 
-    def _flattenObjectsInSelectionList(self, selectionListIter, strategy):
+    def _flattenObjectsInSelectionList(self, selectionListIter):
         while not selectionListIter.isDone():
             dagPath = om.MDagPath()
             components = om.MObject()
             selectionListIter.getDagPath(dagPath, components)
             print('flattening selection on object %(object)s' % {'object': dagPath.partialPathName()})
-
-            connectedFaceSets = findConnectedFaces(dagPath, components)
-            for connectedFaceSet in connectedFaceSets:
-                if strategy is 'spiral':
-                    tree = createFacetreeSpiral(dagPath, connectedFaceSet)
-                else:
-                    tree = createFacetreeLightning(dagPath, connectedFaceSet)
-                patchBuilder = MeshPatchBuilder()
-                flattenTree(dagPath, tree, patchBuilder)
-            selectionListIter.next()
+            self._flattenObject(dagPath, components)
             print('flattening faces for selected object ... done')
+            selectionListIter.next()
+
+    def _flattenObject(self, dagPath, components):
+        connectedFaceSets = findConnectedFaces(dagPath, components)
+        for connectedFaceSet in connectedFaceSets:
+            tree = createFacetreeLightning(dagPath, connectedFaceSet)
+            patchBuilder = MeshPatchBuilder()
+            flattenTree(dagPath, tree, patchBuilder)
