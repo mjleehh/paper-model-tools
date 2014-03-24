b733db4977ed11d2163918f4605e760059a3f517
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-03-24 23:45:46 +0100

    Improved mesh edge class.

--- unfolder/util/mesh_faces.py
+++ unfolder/util/mesh_faces.py
@@ -25,14 +25,36 @@ class MeshFace:
         self._dagPath = dagPath
         self.index = index
 
-    def getEdgeIndices(self):
+    def __len__(self):
+        return self._getEdgeIndices().length()
+
+    def getConnectingEdges(self, otherFace):
+        sharedEdgeIndices = frozenset(self._getEdgeIndices()) & frozenset(otherFace._getEdgeIndices())
+        return [MeshEdge(self._dagPath, index) for index in sharedEdgeIndices]
+
+    def _getEdgeIndices(self):
         retval = om.MIntArray()
-        self._getFaceIter(self.index).getEdges(retval)
+        self._getFaceIter().getEdges(retval)
         return retval
 
-    def _getFaceIter(self, faceIndex):
-        return setIter(om.MItMeshPolygon(), faceIndex)
+    def _getFaceIter(self):
+        return setIter(om.MItMeshPolygon(), self.index)
 
+class MeshEdge:
+    def __init__(self, dagPath, index):
+        self._dagPath = dagPath
+        self.index = index
+
+    def getConnectedFaces(self):
+        return [MeshFace(self._dagPath, index) for index in self._getConnectedFaceIndices()]
+
+    def _getConnectedFaceIndices(self):
+        retval = om.MIntArray()
+        self._getEdgeIter().getConnectedFaces(retval)
+        return retval
+
+    def _getEdgeIter(self):
+        return setIter(om.MItMeshEdge, self.index)
 
 def setIter(iter, index):
     prevIndex = om.MScriptUtil()
