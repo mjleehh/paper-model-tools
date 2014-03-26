0768e6e09cfdc34f9c770473039f504493b2ea15
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-03-27 00:31:06 +0100

    Basic obj-file importer added.

--- unfolder/util/obj_mesh.py
+++ unfolder/util/obj_mesh.py
@@ -3,20 +3,51 @@ class ObjectFileReader:
     def __init__(self):
         self.vertices = []
         self.normals = []
-        self.edges = []
         self.faces = []
-        self.initHandlers()
+        self._handlers = self._createHandlers()
 
-    def initHandlers(self):
+    def _createHandlers(self):
+
+        def v(x, y, z):
+            self.vertices.append((float(x), float(y), float(z)))
+
+        def vn(x, y, z):
+            self.normals.append((float(x), float(y), float(z)))
+
+        def determineFaceMode(coords):
+            parts = coords[0].split('/')
+            textureCoords = [] if len(parts) > 1 and parts[1] else None
+            normals = [] if len(parts) > 2 else None
+            return textureCoords, normals
+
+        def f(*coords):
+            vertices = []
+            textureCoords, normals = determineFaceMode(coords)
+
+            for coord in coords:
+                splitCoords = coord.split('/')
+                print(splitCoords)
+                vertices.append(int(splitCoords[0]))
+                if textureCoords is not None:
+                    textureCoords.append(int(splitCoords[1]))
+                if normals is not None:
+                    normals.append(int(splitCoords[2]))
+
+            self.faces.append((vertices, textureCoords, normals))
+
+        return {'v': v, 'vn': vn, 'f': f}
+
+    def handle(self, verb, nouns):
+        if verb in self._handlers:
+            self._handlers[verb](*nouns)
 
     def read(self,  file):
         with open(file) as f:
             for line in f:
                 self.handleLine(line)
 
-
     def handleLine(self, line):
         words = line.split()
         if words:
             verb = words[0]
-            Hanlder()
+            self.handle(verb, words[1:])
