a787ae685d02a7a616763e60074043aba65f9af6
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-03-27 09:20:29 +0100

    Restructure obj import to use a vertex-edge-face MESH representation.

--- unfolder/util/obj_mesh.py
+++ unfolder/util/obj_mesh.py
@@ -1,41 +1,24 @@
+class ObjMesh():
+    def __init__(self):
+        self.vertices = []
+        self.edges = []
+        self.faces = []
+
+class ObjFace():
+    def __init__(self, edges, textureCoords):
+        self.edges = edges
+        self.textureCoords = textureCoords
+
 class ObjectFileReader:
 
     def __init__(self):
-        self.vertices = []
-        self.normals = []
-        self.faces = []
+        self._mesh = ObjMesh()
+        self._edgeMapping = {}
         self._handlers = self._createHandlers()
 
     def _createHandlers(self):
 
-        def v(x, y, z):
-            self.vertices.append((float(x), float(y), float(z)))
-
-        def vn(x, y, z):
-            self.normals.append((float(x), float(y), float(z)))
-
-        def determineFaceMode(coords):
-            parts = coords[0].split('/')
-            textureCoords = [] if len(parts) > 1 and parts[1] else None
-            normals = [] if len(parts) > 2 else None
-            return textureCoords, normals
-
-        def f(*coords):
-            vertices = []
-            textureCoords, normals = determineFaceMode(coords)
-
-            for coord in coords:
-                splitCoords = coord.split('/')
-                print(splitCoords)
-                vertices.append(int(splitCoords[0]))
-                if textureCoords is not None:
-                    textureCoords.append(int(splitCoords[1]))
-                if normals is not None:
-                    normals.append(int(splitCoords[2]))
-
-            self.faces.append((vertices, textureCoords, normals))
-
-        return {'v': v, 'vn': vn, 'f': f}
+        return {'v': v, 'f': f}
 
     def handle(self, verb, nouns):
         if verb in self._handlers:
@@ -51,3 +34,50 @@ class ObjectFileReader:
         if words:
             verb = words[0]
             self.handle(verb, words[1:])
+
+
+    # handlers
+
+    def v(self, x, y, z):
+        self._mesh.vertices.append((float(x), float(y), float(z)))
+
+    def f(self, *coords):
+        edges = []
+        textureCoords, normals = self._determineFaceMode(coords)
+
+        prevVertex = None
+        for coord in coords:
+            splitCoords = coord.split('/')
+
+            if textureCoords is not None:
+                textureCoords.append(int(splitCoords[1]))
+
+            vertex = (int(splitCoords[0]))
+
+            # make relative vertex positions absolute
+            if vertex < 0:
+                vertex += len(self._mesh.vertices)
+
+            if prevVertex:
+                edges.append(self.addEdge(prevVertex, vertex))
+
+            prevVertex = vertex
+
+        self._mesh.faces.append(ObjFace(edges, textureCoords))
+
+    def addEdge(self, fst, snd):
+        key = frozenset((fst, snd))
+        if key in self._edgeMapping:
+            return self._edgeMapping[key]
+        else:
+            index = len(self._mesh.edges)
+            self._mesh.edges.append((fst, snd))
+            self._edgeMapping[key] = index
+            return index
+
+    @staticmethod
+    def _determineFaceMode(coords):
+            parts = coords[0].split('/')
+            textureCoords = [] if len(parts) > 1 and parts[1] else None
+            normals = [] if len(parts) > 2 else None
+            return textureCoords, normals
