85870f086b46f8f1bb1bab17ab74207fceebbe17
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-03-27 23:00:23 +0100

    Added new model command to plugin.

--- paper_model_tools_plugin.py
+++ paper_model_tools_plugin.py
@@ -2,6 +2,7 @@ import maya.OpenMayaMPx as omp
 import sys
 
 from unfolder.automatic_unfold.create_paper_model_command import CreatePaperModelCommand
+from unfolder.automatic_unfold.create_paper_model_command2 import CreatePaperModelCommand2
 from unfolder.select_facetree.create_paper_model_tool import CreatePaperModelTool
 
 
@@ -13,9 +14,13 @@ def createCommand():
     return omp.asMPxPtr(CreatePaperModelCommand())
 
 
+def createCommand2():
+    print('blub')
+    return omp.asMPxPtr(CreatePaperModelCommand2())
+
 kToolCommandName = "createPaperModelTool"
 kCommandName = "createPaperModel"
-
+kCommandName2 = "createPaperModel2"
 
 # Initialize the script plug-in
 def initializePlugin(mobject):
@@ -27,6 +32,7 @@ def initializePlugin(mobject):
     try:
         mplugin.registerContextCommand(kToolCommandName, createToolCommand)
         mplugin.registerCommand(kCommandName, createCommand)
+        mplugin.registerCommand(kCommandName2, createCommand2)
     except:
         sys.stderr.write('Failed to register commands')
         raise
@@ -40,5 +46,6 @@ def uninitializePlugin(mobject):
     try:
         mplugin.deregisterContextCommand(kToolCommandName)
         mplugin.deregisterCommand(kCommandName)
+        mplugin.deregisterCommand(kCommandName2)
     except:
         sys.stderr.write('Failed to unregister commands')
