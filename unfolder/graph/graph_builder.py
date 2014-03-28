35997457e4a37150b3553829e53f88e7d3a2cbcc
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-03-28 23:43:47 +0100

    Added redundant referencing to improve graph builder efficiency.

--- unfolder/graph/graph_builder.py
+++ unfolder/graph/graph_builder.py
@@ -4,36 +4,49 @@ from .graph import Graph, GraphEdge
 class GraphBuilder:
     def __init__(self):
         self._nodes = []
-        self._edges = {}
+        self._nodeMap = {}
+        self._edgeLists = []
+        self._edges = []
+        self._edgeMap = {}
 
     def addNode(self, value, connectedValues):
-        self._createNode(value)
-        for otherValue in self._findNewConnections(value, connectedValues):
-            self._createNode(otherValue)
-            self._createEdge(value, otherValue)
-
+        thisIndex = self._createNode(value)
+        for otherValue in self._findNewConnections(thisIndex, connectedValues):
+            otherIndex = self._createNode(otherValue)
+            self._createEdge(thisIndex, otherIndex)
 
     # private
 
-    def _findNewConnections(self, node, connectedValues):
-        currentlyConnectedValues = frozenset(self._getConnectedNodes(node))
+    def _findNewConnections(self, nodeIndex, connectedValues):
+        currentlyConnectedValues = frozenset(self._getConnectedNodes(nodeIndex))
         return frozenset(connectedValues) - currentlyConnectedValues
 
-    def _getConnectedNodes(self, node):
-        return [edge.getOther(node) for edge in self._edges.values() if edge.hasNode(node)]
+    def _getConnectedEdges(self, nodeIndex):
+        return [self._edges[edgeIndex] for edgeIndex in self._edgeLists[nodeIndex]]
+
+    def _getConnectedNodes(self, nodeIndex):
+        return [edge.getOther(nodeIndex) for edge in self._getConnectedEdges(nodeIndex)]
 
     def _createNode(self, value):
-        if value not in self._nodes:
-            self._nodes.append(value)
-
-    def _createEdge(self, firstNode, secondNode):
-        edge = GraphEdge(firstNode, secondNode)
-        edgeKey = hash(edge)
-        if edgeKey in self._edges:
-            return self._edges[edgeKey]
+        if value in self._nodeMap:
+            return self._nodeMap[value]
         else:
-            self._edges[edgeKey] = edge
-            return edge
+            index = len(self._nodes)
+            self._nodes.append(value)
+            self._nodeMap[value] = index
+            self._edgeLists.append([])
+            return index
+
+    def _createEdge(self, firstIndex, secondIndex):
+        edge = GraphEdge(firstIndex, secondIndex)
+        if edge in self._edgeMap:
+            return self._edgeMap[edge]
+        else:
+            index = len(self._edges)
+            self._edges.append(edge)
+            self._edgeLists[firstIndex].append(index)
+            self._edgeLists[secondIndex].append(index)
+            return index
 
     def toGraph(self):
         return Graph(self._nodes, self._edges)
