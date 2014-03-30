59e65ab7e0f9c8d5f3b6d286d282ded63aa9681f
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-03-30 18:50:39 +0200

    Added cloning and counting to graphs.

--- unfolder/graph/graph.py
+++ unfolder/graph/graph.py
@@ -4,6 +4,11 @@ class Graph:
         self.nodes = nodes
         self.edges = edges
 
+    def numNodes(self):
+        return len(self.nodes)
+
+    def numEdges(self):
+        return len(self.edges)
 
     def isConnected(self):
         return len(self._getConnectedNodes()) == 1
@@ -13,6 +18,9 @@ class Graph:
 
     def edgeIndex(self, edge):
         return self.edges.index(edge)
+
+    def clone(self):
+        return Graph(self.nodes[:], self.edges[:])
 
     def getConnectedSubgraphs(self):
         retval = []
@@ -25,6 +33,60 @@ class Graph:
                 for edge in self.getConnectedEdges(node):
                     edges[edge] = edge
             retval.append(Graph(nodes, edges))
+        return retval
+
+    def isTree(self):
+        def traverse(nodeIndex, parentIndex):
+            if visitedNodes[nodeIndex]:
+                return True
+
+            visitedNodes[nodeIndex] = True
+            for connectedNodeIndex in self._getConnectedNodeIndices(nodeIndex):
+                if connectedNodeIndex != parentIndex:
+                    if traverse(connectedNodeIndex, nodeIndex):
+                        return True
+
+            return False
+
+        visitedNodes = [False] * len(self.nodes)
+        if traverse(0, None):
+            return False
+
+        for visitedNode in visitedNodes:
+            if not visitedNode:
+                return False
+
+        return True
+
+    def getSpanningTree(self, rootNodeIndex = 0):
+        def traverse(nodeIndex, parentIndex):
+            if notVisitedNodes[nodeIndex]:
+                notVisitedNodes[nodeIndex] = False
+                if parentIndex is not None:
+                    edges.append(GraphEdge(parentIndex, nodeIndex))
+                for connectedNodeIndex in self._getConnectedNodeIndices(nodeIndex):
+                    if connectedNodeIndex != parentIndex:
+                        traverse(connectedNodeIndex, nodeIndex)
+
+        edges = []
+        notVisitedNodes = [True] * len(self.nodes)
+        traverse(rootNodeIndex, None)
+        if len(edges) != len(self.nodes) - 1:
+            raise ValueError('graph is not connected!')
+        return Graph(self.nodes, edges)
+
+    def __repr__(self):
+        retval = 'G = (\n  V = {'
+        delim = ''
+        for node in self.nodes:
+            retval += delim + repr(node)
+            delim = ', '
+        retval += '},\n  E = {'
+        delim = ''
+        for edge in self.edges:
+            retval += delim + repr(edge)
+            delim = ', '
+        retval += "}\n)"
         return retval
 
     # private
@@ -57,61 +119,7 @@ class Graph:
             connectedGraphs.append(findConnectedGraph(remainingNodes))
         return connectedGraphs
 
-    def isTree(self):
-        def traverse(nodeIndex, parentIndex):
-            if visitedNodes[nodeIndex]:
-                return True
 
-            visitedNodes[nodeIndex] = True
-            for connectedNodeIndex in self._getConnectedNodeIndices(nodeIndex):
-                if connectedNodeIndex != parentIndex:
-                    if traverse(connectedNodeIndex, nodeIndex):
-                        return True
-
-            return False
-
-        visitedNodes = [False] * len(self.nodes)
-        if traverse(0, None):
-            return False
-
-        for visitedNode in visitedNodes:
-            if not visitedNode:
-                return False
-
-        return True
-
-
-    def getSpanningTree(self, rootNodeIndex = 0):
-
-        def traverse(nodeIndex, parentIndex):
-            if notVisitedNodes[nodeIndex]:
-                notVisitedNodes[nodeIndex] = False
-                if parentIndex is not None:
-                    edges.append(GraphEdge(parentIndex, nodeIndex))
-                for connectedNodeIndex in self._getConnectedNodeIndices(nodeIndex):
-                    if connectedNodeIndex != parentIndex:
-                        traverse(connectedNodeIndex, nodeIndex)
-
-        edges = []
-        notVisitedNodes = [True] * len(self.nodes)
-        traverse(rootNodeIndex, None)
-        if len(edges) != len(self.nodes) - 1:
-            raise ValueError('graph is not connected!')
-        return Graph(self.nodes, edges)
-
-    def __repr__(self):
-        retval = 'G = (\n  V = {'
-        delim = ''
-        for node in self.nodes:
-            retval += delim + repr(node)
-            delim = ', '
-        retval += '},\n  E = {'
-        delim = ''
-        for edge in self.edges:
-            retval += delim + repr(edge)
-            delim = ', '
-        retval += "}\n)"
-        return retval
 
 
 class GraphEdge:
@@ -121,15 +129,18 @@ class GraphEdge:
         self.nodeIndices = (fstIndex, sndIndex) if fstIndex < sndIndex else (sndIndex, fstIndex)
 
     def hasNode(self, nodeIndex):
-        return self._fst() == nodeIndex or self._snd() == nodeIndex
+        return self.fst() == nodeIndex or self.snd() == nodeIndex
 
     def getOther(self, nodeIndex):
-        if self._fst() == nodeIndex:
-            return self._snd()
-        elif self._snd() == nodeIndex:
-            return self._fst()
+        if self.fst() == nodeIndex:
+            return self.snd()
+        elif self.snd() == nodeIndex:
+            return self.fst()
         else:
             return None
+
+    def __eq__(self, other):
+        return self.nodeIndices == other.nodeIndices
 
     def __hash__(self):
         return hash(self.nodeIndices)
@@ -137,10 +148,8 @@ class GraphEdge:
     def __repr__(self):
         return str(self.nodeIndices)
 
-    # private
-
-    def _fst(self):
+    def fst(self):
         return self.nodeIndices[0]
 
-    def _snd(self):
+    def snd(self):
         return self.nodeIndices[1]
