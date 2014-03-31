63294e194a728ea275c92b6afd3c7de7ed921f1f
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-03-31 08:52:17 +0200

    Improved unit tests for spanning tree iteration.

--- unfolder/graph/test/spanning_trees.py
+++ unfolder/graph/test/spanning_trees.py
@@ -1,16 +1,33 @@
 from unittest import TestCase
+from unfolder.graph.graph import GraphEdge
 from unfolder.graph.graph_builder import GraphBuilder
-from unfolder.graph.spanning_trees import spanningTrees
+from unfolder.graph.spanning_trees import SpanningTrees
+from unfolder.graph.test.sample_graphs import createGraphWithTreeSpanningTrees, createSimpleGraph, \
+    createSingularGraph, createPrimitiveGraph, createEmptyGraph, \
+    createSimpleTree, createDiamondGraph
 
 
 class TestSpanningTrees(TestCase):
-    def test_spanningTrees(self):
-        graphBuilder = GraphBuilder()
-        graphBuilder.addNode('A', ['B', 'D'])
-        graphBuilder.addNode('B', ['A', 'C', 'D'])
-        graphBuilder.addNode('C', ['B', 'D'])
-        graphBuilder.addNode('D', ['A', 'B', 'C'])
-        graph = graphBuilder.toGraph()
 
-        for spanningTree in spanningTrees(graph):
-            pass
+    def test_spanningTrees(self):
+
+        testCases = [
+            (createEmptyGraph(), 1),
+            (createSingularGraph(), 1),
+            (createPrimitiveGraph(), 1),
+            (createSimpleTree(), 1),
+            (createGraphWithTreeSpanningTrees(), 3),
+            (createSimpleGraph(), 8),
+            (createDiamondGraph(), 16)]
+
+        for testCase in testCases:
+            (graph, numSpanningTrees) = testCase
+
+            spanningTreeEdgeSets = [frozenset(spanningTree.edges) for spanningTree in SpanningTrees(graph)]
+
+            # check for correct number of spanning trees
+            self.assertEqual(len(spanningTreeEdgeSets), numSpanningTrees)
+
+            # check there are no spanning tree duplicates
+            self.assertEqual(len(set(spanningTreeEdgeSets)), numSpanningTrees)
+
