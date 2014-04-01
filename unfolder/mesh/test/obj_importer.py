c1b838814e7eb753a456e9e0360bc36591f13eb9
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-04-02 00:19:45 +0200

    Completed OBJ importer test.

--- unfolder/mesh/test/obj_importer.py
+++ unfolder/mesh/test/obj_importer.py
@@ -3,7 +3,7 @@ from unfolder.mesh.obj_importer import ObjImporter
 from unfolder.mesh.obj_mesh import ObjEdge
 
 
-class TestGraph(TestCase):
+class ObjImporterTests(TestCase):
 
     def test_importObj_box(self):
         reader = ObjImporter()
@@ -15,18 +15,21 @@ class TestGraph(TestCase):
         self.assertEqual(box.vertices, expectedVertices)
 
         # texture coordinates are missing
-        self.fail()
+
+        expectedTextureCoordinates = [(0.375, 0.0), (0.625, 0.0), (0.375, 0.25), (0.625, 0.25), (0.375, 0.5), (0.625, 0.5), (0.375, 0.75), (0.625, 0.75), (0.375, 1.0), (0.625, 1.0), (0.875, 0.0), (0.875, 0.25), (0.125, 0.0), (0.125, 0.25)]
+        self.assertEqual(box.textureCoords, expectedTextureCoordinates)
 
         # check if faces have been imported correctly
 
         self.assertEqual(len(box.faces), 6)
         expectedFaceEdgeLists = [[0, 1, 2, 3], [2, 4, 5, 6], [5, 7, 8, 9], [8, 10, 0, 11], [10, 7, 4, 1], [11, 3, 6, 9]]
+        expectedFaceTextureCoordList = [[0, 1, 3, 2], [2, 3, 5, 4], [4, 5, 7, 6], [6, 7, 9, 8], [1, 10, 11, 3], [12, 0, 2, 13]]
         for faceIndex, face in enumerate(box.faces):
             self.assertEqual(face.edges, expectedFaceEdgeLists[faceIndex])
-
+            self.assertEqual(face.textureCoords, expectedFaceTextureCoordList[faceIndex])
         # check if edges have been imported correctly
 
         self.assertEqual(len(box.edges), 12)
-        edgeTuples = [(1, 2), (2, 4), (3, 4), (1, 3), (4, 6), (5, 6), (3, 5), (6, 8), (7, 8), (5, 7), (2, 8), (1, 7)]
+        edgeTuples = [(0, 1), (1, 3), (2, 3), (0, 2), (3, 5), (4, 5), (2, 4), (5, 7), (6, 7), (4, 6), (1, 7), (0, 6)]
         expectedEdges = [ObjEdge(fst, snd) for (fst, snd) in edgeTuples]
         self.assertEqual(box.edges, expectedEdges)
