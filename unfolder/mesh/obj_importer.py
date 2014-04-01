de60e9fd2e3c013916b7f700056cac5f8060a864
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-04-02 00:19:20 +0200

    Fixed index bug: OBJ files use indices starting with at 1!

--- unfolder/mesh/obj_importer.py
+++ unfolder/mesh/obj_importer.py
@@ -11,23 +11,13 @@ class ObjImporter:
         self._edgeMapping = {}
         self._vertices = []
         self._textureCoords = []
-        self._handlers = {'v': self.v, 'f': self.f}
-
-    def handle(self, verb, nouns):
-        if verb in self._handlers:
-            self._handlers[verb](*nouns)
+        self._handlers = {'v': self.v, 'vt' : self.vt, 'f': self.f}
 
     def read(self,  file):
         with open(file) as f:
             for line in f:
-                self.handleLine(line)
+                self._handleLine(line)
         return ObjMesh(self._faces, self._edges, self._vertices, self._textureCoords)
-
-    def handleLine(self, line):
-        words = line.split()
-        if words:
-            verb = words[0]
-            self.handle(verb, words[1:])
 
     # handlers
 
@@ -35,38 +25,53 @@ class ObjImporter:
         self._vertices.append((float(x), float(y), float(z)))
 
     def f(self, *coords):
-        textureCoords = self.getTextureCoords(coords)
-        edges = self.getEdges(coords)
+        textureCoords = self._getTextureCoords(coords)
+        edges = self._getEdges(coords)
         self._faces.append(ObjFace(edges, textureCoords))
 
-    def getTextureCoords(self, coords):
+    def vt(self, x, y):
+        self._textureCoords.append((float(x), float(y)))
+
+    # private
+
+    def _handle(self, verb, nouns):
+        if verb in self._handlers:
+            self._handlers[verb](*nouns)
+
+    def _handleLine(self, line):
+        words = line.split()
+        if words:
+            verb = words[0]
+            self._handle(verb, words[1:])
+
+    def _getTextureCoords(self, coords):
         parts = coords[0].split('/')
         if len(parts) > 1 and parts[1]:
-            return [self.getTextureCoord(coord) for coord in coords]
+            return [self._getTextureCoord(coord) for coord in coords]
         return None
 
-    def getTextureCoord(self, tuple):
-        textureCoord = int(tuple.split('/')[1])
-        return textureCoord if textureCoord > 0 else textureCoord + len(self._textureCoords)
+    def _getTextureCoord(self, tuple):
+        textureCoord = int(tuple.split('/')[1]) - 1
+        return textureCoord if textureCoord >= 0 else textureCoord + len(self._textureCoords)
 
-    def getVertex(self, tuple):
-        vertexCoord = int(tuple.split('/')[0])
-        return vertexCoord if vertexCoord > 0 else vertexCoord + len(self._vertices)
+    def _getVertex(self, tuple):
+        vertexCoord = int(tuple.split('/')[0]) - 1
+        return vertexCoord if vertexCoord >= 0 else vertexCoord + len(self._vertices)
 
-    def getEdges(self, coords):
-        vertices = [self.getVertex(vertex) for vertex in coords]
+    def _getEdges(self, coords):
+        vertices = [self._getVertex(vertex) for vertex in coords]
         prevVertex = vertices[0]
         vertices = vertices[1:] + [prevVertex]
         edges = []
 
         for vertex in vertices:
-            edgeIndex = self.addEdge(prevVertex, vertex)
+            edgeIndex = self._addEdge(prevVertex, vertex)
             self._edges[edgeIndex].faces.append(len(self._faces))
             edges.append(edgeIndex)
             prevVertex = vertex
         return edges
 
-    def addEdge(self, fst, snd):
+    def _addEdge(self, fst, snd):
         edge = ObjEdge(fst, snd)
         key = hash(edge)
         if key in self._edgeMapping:
