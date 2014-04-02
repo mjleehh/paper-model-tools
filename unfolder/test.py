4fc321603625e942688891b51c12e2a2b27be5b3
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-04-02 09:27:06 +0200

    Added little testing script for new workflow.

---
+++ unfolder/test.py
@@ -0,0 +1,28 @@
+from unfolder.automatic_unfold.mesh_to_graph import meshToGraph
+from unfolder.graph.Tree import graphToTree
+from unfolder.graph.graph_builder import GraphBuilder
+from unfolder.graph.spanning_trees import SpanningTrees
+from unfolder.mesh.obj_importer import ObjImporter
+from unfolder.mesh.obj_mesh import MeshFaces
+
+def printTree(tree, depth=0):
+    res = ' ' * depth
+    res += str(tree.value)
+    print(res)
+    for child in tree:
+        printTree(child, depth + 1)
+
+mesh = ObjImporter().read('mesh/test/resources/box.obj')
+faces = MeshFaces(mesh)
+graph = meshToGraph(faces, GraphBuilder())
+connectedComponents = graph.getConnectedComponents()
+
+for connectedComponent in connectedComponents:
+    for index, spanningTree in enumerate(SpanningTrees(connectedComponent)):
+        print('spanningTree no %i' % index)
+        tree = graphToTree(spanningTree)
+        printTree(tree)
+
+
+
+
