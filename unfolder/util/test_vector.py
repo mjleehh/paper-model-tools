1fd6d6296b4d201219b48a8c066a022b499e7b21
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-04-04 21:22:22 +0200

    Improved vector class.

--- unfolder/util/test_vector.py
+++ unfolder/util/test_vector.py
@@ -1,9 +1,136 @@
+from copy import copy
 from unittest import TestCase
 from unfolder.util.vector import Vector
 
 import numpy.linalg as lg
 
 class TestVector(TestCase):
+    def test_add_2d(self):
+        v1 = Vector(1, 2)
+        v2 = Vector(77, 12)
+
+        v1plusv2 = Vector(78, 14)
+        self.assertEqual(v1 + v2, v1plusv2)
+
+        v3 = copy(v1)
+        v3 += v2
+        self.assertEqual(v3, v1plusv2)
+
+    def test_add_3d(self):
+        v1 = Vector(23, 493, 2)
+        v2 = Vector(3, -23, 8)
+
+        v1plusv2 = Vector(26, 470, 10)
+        self.assertEqual(v1 + v2, Vector(v1plusv2))
+
+        v3 = copy(v1)
+        v3 += v2
+        self.assertEqual(v3, v1plusv2)
+
+    def test_add_other_iterable(self):
+        v1 = Vector(3, 25, 2, 78)
+        v2 = (8, 32, -9, 4)
+
+        v1plusv2 = Vector(11, 57, -7, 82)
+        self.assertEqual(v1 + v2, v1plusv2)
+
+        v3 = copy(v1)
+        v3 += v2
+        self.assertEqual(v3, v1plusv2)
+
+    def test_sub_2d(self):
+        v1 = Vector(4, 1, 7)
+        v2 = Vector(77, 12, 5)
+
+        v1minusv2 = Vector(-73, -11, 2)
+        self.assertEqual(v1 - v2, v1minusv2)
+
+        v3 = copy(v1)
+        v3 -= v2
+        self.assertEqual(v3, v1minusv2)
+
+    def test_sub_3d(self):
+        v1 = Vector(3, 8, 11)
+        v2 = (-1, 18, 0)
+
+        v1minusv2 = Vector(4, -10, 11)
+        self.assertEqual(v1 - v2, Vector(v1minusv2))
+
+        v3 = copy(v1)
+        v3 -= v2
+        self.assertEqual(v3, v1minusv2)
+
+    def test_sub_other_iterable(self):
+        v1 = Vector(12, 8, 5, 1)
+        v2 = (2, 0, 32, -1)
+
+        v1minusv2 = Vector(10, 8, -27, 2)
+        self.assertEqual(v1 - v2, v1minusv2)
+
+        v3 = copy(v1)
+        v3 -= v2
+        self.assertEqual(v3, v1minusv2)
+
+    def test_mul_2d(self):
+        v1 = Vector(3, 4)
+        v2 = Vector(2, 7)
+
+        v1dotv2 = 34
+        self.assertEqual(v1 * v2, v1dotv2)
+
+        v3 = copy(v1)
+        v3 *= v2
+        self.assertEqual(v3, v1dotv2)
+
+    def test_mul_3d(self):
+        v1 = Vector(2, 4, 6)
+        v2 = Vector(4, -3, 5)
+
+        v1dotv2 = 26
+        self.assertEqual(v1 * v2, v1dotv2)
+
+        v3 = copy(v1)
+        v3 *= v2
+        self.assertEqual(v3, v1dotv2)
+
+    def test_mul_other_iterable(self):
+        v1 = Vector(2, -3, 9, 8)
+        v2 = (2, 0, 7, -5)
+
+        v1dotv2 = 27
+        self.assertEqual(v1 * v2, v1dotv2)
+
+        v3 = copy(v1)
+        v3 *= v2
+        self.assertEqual(v3, v1dotv2)
+
+    def test_div(self):
+        v1 = Vector(3, 1)
+        s1 = 7
+        v1overs1 = Vector(21, 7)
+        self.assertEqual(v1 / s1, v1overs1)
+
+        v3 = copy(v1)
+        v3 /=  s1
+        self.assertEqual(v3, v1overs1)
+
+    def test_crossProduct(self):
+        v1 = Vector(1, 0, 0)
+
+        v2 = Vector(0, 1, 0)
+        v1xv2 = Vector(0, 0, 1)
+        self.assertEqual(v1 ^ v2, v1xv2)
+
+        v3 = copy(v1)
+        v3 ^= v2
+        self.assertEqual(v3, v1xv2)
+
+        v2xv1 = Vector(0, 0, -1)
+        self.assertEqual(v2 ^ v1, v2xv1)
+        v4 = copy(v2)
+        v4 ^= v1
+        self.assertEqual(v4, v2xv1)
+
     def test_normalize(self):
         v = Vector(2., 5., 8., 5.)
         v.normalize()
@@ -17,6 +144,17 @@ class TestVector(TestCase):
         v2 = Vector(v2Coords)
         self.assertEqual(v2.norm(), lg.norm(v2Coords))
 
+    def test_equal(self):
+        v1 = Vector(1, 2, 4)
+        v2 = Vector(2, 7, 8)
+        v3 = Vector(1, 2, 4)
+
+        self.assertTrue(v1 != v2)
+        self.assertFalse(v1 == v2)
+
+        self.assertTrue(v1 == v3)
+        self.assertFalse(v1 != v3)
+
     def test_getItem(self):
         items = (2, 4, 5, 9, 123)
         v1 = Vector(items)
@@ -27,3 +165,16 @@ class TestVector(TestCase):
         v2 = Vector(*items)
         for index, item in enumerate(items):
             self.assertEqual(item, v2[index])
+
+    def test_len(self):
+        v1 = Vector(1)
+        self.assertEqual(len(v1), 1)
+
+        v2 = Vector(1, 2)
+        self.assertEqual(len(v2), 2)
+
+        v3 = Vector(1, 2, 3)
+        self.assertEqual(len(v3), 3)
+
+        v4 = Vector(1, 2, 3, 4)
+        self.assertEqual(len(v4), 4)
--- unfolder/util/vector.py
+++ unfolder/util/vector.py
@@ -1,25 +1,39 @@
 import numpy as np
 import numpy.linalg as lg
 
+
 class Vector:
     def __init__(self, *v):
+        if len(v) < 1:
+            raise DimensionError('Vector must have finite dimension!')
         if len(v) == 1:
             self.v = np.array(v[0])
         else:
             self.v = np.array(v)
 
     def __add__(self, other):
-        self.v + other
+        if not self._hasSameDim(other):
+            raise DimensionError('Can not sum vectors with different dimensions!')
+        return Vector(self.v + other.v) if isVector(other) else Vector(self.v + other)
 
     def __sub__(self, other):
-        self.v - other
+        if not self._hasSameDim(other):
+            raise DimensionError('Can not subtract vectors with different dimensions!')
+        return Vector(self.v - other.v) if isVector(other) else Vector(self.v - other)
 
     def __mul__(self, other):
         # works for vectors and scalars
-        np.dot(self.v, other)
+        return np.dot(self.v, other)
 
-    def __pow__(self, other, modulo=None):
-        return np.cross(self.v, other)
+    def __div__(self, scalar):
+        if not isinstance(scalar, (int, float, complex)):
+            raise DimensionError('Divisor must be scalar!')
+        return Vector(self.v / scalar)
+
+    def __xor__(self, other):
+        if not self._hasSameDim(other):
+            raise DimensionError('Can not cross multiply vectors with different dimensions')
+        return Vector(np.cross(self.v, other))
 
     def norm(self):
         return lg.norm(self.v)
@@ -27,8 +41,23 @@ class Vector:
     def normalize(self):
         self.v /= self.norm()
 
+    def __len__(self):
+        return self.v.size
+
     def __getitem__(self, item):
         return self.v[item]
+
+    def __iter__(self):
+        for item in self.v:
+            yield item
+
+    def __eq__(self, other):
+        if len(self.v) != len(other):
+            raise DimensionError('Can not compare two vectors of different dimension')
+        for index, item in enumerate(self):
+            if item != other[index]:
+                return False
+        return True
 
     def __repr__(self):
         res = '('
@@ -40,3 +69,14 @@ class Vector:
         res += ')'
         return res
 
+    # private
+
+    def _hasSameDim(self, v):
+        return len(self) == len(v)
+
+def isVector(x):
+    return isinstance(x, Vector)
+
+class DimensionError(Exception):
+    def __init__(self, msg):
+        super(msg)
