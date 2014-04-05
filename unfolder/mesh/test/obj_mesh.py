061844d34a0475162c2d6b9ad646c4356dd8473b
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-04-06 00:22:21 +0200

    Face edge accessor tests added.

--- unfolder/mesh/test/obj_mesh.py
+++ unfolder/mesh/test/obj_mesh.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 from unfolder.mesh.obj_importer import ObjImporter
 from unfolder.mesh.obj_mesh import ObjEdge, MeshFaces
+from unfolder.util.vector import Vector
 
 
 class MeshFacesTests(TestCase):
-
     @classmethod
     def setUpClass(cls):
         reader = ObjImporter()
@@ -32,7 +32,7 @@ class MeshFacesTests(TestCase):
             self.assertEqual(i, face.index)
 
 
-class MeshFaceTest(TestCase):
+class MeshFaceTests(TestCase):
     @classmethod
     def setUpClass(cls):
         reader = ObjImporter()
@@ -49,3 +49,64 @@ class MeshFaceTest(TestCase):
     def test_getVertices(self):
         expectedFaceVertices = [(0.999997, -35.355339, 82.710678), (71.710678, -35.355339, 12.0), (1.0, 35.355339, 12.0)]
         self.assertEqual(expectedFaceVertices, self.face.getVertices())
+
+    def test_getNormal(self):
+        normal = self.face.getNormal()
+
+
+class MeshFaceEdgeTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        reader = ObjImporter()
+        mesh = reader.read('resources/pyramid.obj')
+        cls.meshFaceEdges = MeshFaces(mesh)[3].edges
+        cls.expectedEdges = (1, 7, 6)
+
+    def test_getitem(self):
+        e1 = self.meshFaceEdges[0]
+        self.assertEqual(e1.index, 1)
+        print(e1.index)
+
+        e2 = self.meshFaceEdges[1]
+        self.assertEqual(e2.index, 7)
+
+        e3 = self.meshFaceEdges[2]
+        self.assertEqual(e3.index, 6)
+
+    def test_iter(self):
+        for i, faceEdge in enumerate(self.meshFaceEdges):
+            self.assertEqual(faceEdge.index, self.expectedEdges[i])
+
+    def test_len(self):
+        self.assertEqual(len(self.meshFaceEdges), 3)
+
+
+class MeshEdgeTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        reader = ObjImporter()
+        mesh = reader.read('resources/box.obj')
+        cls.meshFaceEdges = MeshFaces(mesh)[3].edges
+
+    def test_getItem(self):
+        e1 = self.meshFaceEdges[0]
+        v1 = e1[0]
+        self.assertEqual((-73.5, -61.25, -65.400002), v1)
+        v2 = e1[1]
+        self.assertEqual((76.5, -61.25, -65.400002), v2)
+
+        e2 = self.meshFaceEdges[1]
+        v3 = e2[0]
+        self.assertEqual((76.5, -61.25, 65.900002), v3)
+        v4 = e2[1]
+        self.assertEqual((76.5, -61.25, -65.400002), v4)
+
+    def test_direction(self):
+        e1 = self.meshFaceEdges[0]
+        self.assertEqual(e1.direction(), Vector(-150.0, 0.0, 0.0))
+        e2 = self.meshFaceEdges[1]
+        self.assertEqual(e2.direction(), Vector(0.0, 0.0, -131.300004))
+        e3 = self.meshFaceEdges[2]
+        self.assertEqual(e3.direction(), Vector(150.0, 0.0, 0.0))
+        e4 = self.meshFaceEdges[3]
+        self.assertEqual(e4.direction(), Vector(0.0, 0.0, 131.300004))
