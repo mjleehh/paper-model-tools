67b97194e102707f3b3533d759051189d5e03ec2
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-04-08 00:06:16 +0200

    Added a bit of explaination to the test script.

--- unfolder/test.py
+++ unfolder/test.py
@@ -1,10 +1,11 @@
+from unfolder.analyze_patch.model_score import calculateModelScore
 from unfolder.automatic_unfold.mesh_to_graph import meshToGraph
 from unfolder.tree.tree_impl import graphToTree
 from unfolder.graph.graph_builder import GraphBuilder
 from unfolder.graph.spanning_trees import SpanningTrees
 from unfolder.mesh.obj_importer import ObjImporter
 from unfolder.mesh.obj_mesh import MeshFaces
-from unfolder.patch.treeToPatch import treeToPatch
+from unfolder.model.treeToModel import treeToModel
 
 
 def printTree(tree, depth=0):
@@ -14,13 +15,20 @@ def printTree(tree, depth=0):
     for child in tree:
         printTree(child, depth + 1)
 
+# load a sample file
 mesh = ObjImporter().read('mesh/test/resources/box.obj')
+# create an accessor to the mesh faces
 faces = MeshFaces(mesh)
+# create a graph from the mesh's face structure information
+# each graph node holds the index of the corresponding face
 graph = meshToGraph(faces, GraphBuilder())
-connectedComponents = graph.getConnectedComponents()
 
-for connectedComponent in connectedComponents:
+# create a model from every connected set of faces
+for connectedComponent in graph.getConnectedComponents():
+    # iterate all spanning trees of the graph
+    # each spanning tree corresponds to one of the possible models
     for index, spanningTree in enumerate(SpanningTrees(connectedComponent)):
         print('spanningTree no %i' % index)
         tree = graphToTree(spanningTree)
-        treeToPatch(tree, faces)
+        model = treeToModel(tree, faces)
+        score = calculateModelScore(model)
