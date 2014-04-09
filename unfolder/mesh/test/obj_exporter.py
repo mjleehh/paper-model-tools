a1c081927dd16616638ae336171204751858d3e0
Michael Jonathan Lee <code.mjlee@googlemail.com>
2014-04-09 22:28:28 +0200

    Basic test for mesh OBJ exporter added.

---
+++ unfolder/mesh/test/obj_exporter.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from unfolder.mesh.obj_exporter import ObjExporter
+from unfolder.mesh.obj_importer import ObjImporter
+from unfolder.mesh.obj_mesh import Mesh
+from unfolder.mesh.obj_mesh_impl import ObjEdge
+
+
+class ObjImporterTests(TestCase):
+
+    def test_importObj_box(self):
+        reader = ObjImporter()
+        box = reader.read('resources/box.obj')
+
+        writer = ObjExporter('tmp/junk.obj')
+        writer.write(Mesh(box))
